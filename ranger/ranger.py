f9350ba5d0722eddb1dc66653c8d4a06b3ae9be9
+Eevee <eevee@eevee.io>
2019-01-05 22:09:32 -0500

    Create config file and barebones bot thingy.

--- ranger/ranger.py
+++ ranger/ranger.py
@@ -3,14 +3,35 @@ import logging
 import asyncio
 import json
 
+client = discord.Client(max_messages=50000)
+config = None
+
+@client.event
+async def on_ready():
+    print(f"{client.user.name} is logged in.")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
 
 
 if __name__ == '__main__':
-    # Set up logging stuff
+    # Set up logging
     logger = logging.getLogger('discord')
     logger.setLevel(logging.DEBUG)
     handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
     handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
     logger.addHandler(handler)
+
+    with open('config.json', 'r') as fp:
+        config = json.load(fp)
+
+    client.run(config['token'])
+
+
 else:
     print("You are importing the ranger root package as a module. I'm not sure what you are trying to do but you're doing it wrong.")
